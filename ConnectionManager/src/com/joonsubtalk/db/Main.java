23d4e01d5fabe982e847adfc669c38841e38a7ca
joonsubtalk <joonsubtalk@gmail.com>
2013-04-21 15:44:49 -0700

    Scrollable result set with a simple query to state db table

--- ConnectionManager/src/com/joonsubtalk/db/Main.java
+++ ConnectionManager/src/com/joonsubtalk/db/Main.java
@@ -1,13 +1,34 @@
 package com.joonsubtalk.db;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 public class Main {
-
-	/**
-	 * @param args
-	 */
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	private static final String USERNAME = "dbuser";
+	private static final String PASSWORD = "dbpass";
+	private static final String CONN_STRING = 
+			"jdbc:mysql://localhost/explorecalifornia";//localhost may be a ipaddress/dns
+	public static void main(String[] args) throws SQLException {
+		//Class.forName("com.mysql.jdbc.Driver");
+		Connection conn = null;
+		Statement stmnt = null;
+		ResultSet rs = null;
+		
+		try {
+			conn = DriverManager.getConnection(CONN_STRING, USERNAME, PASSWORD);
+			stmnt = conn.createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_READ_ONLY);
+			rs = stmnt.executeQuery("SELECT * FROM states");
+			rs.last();
+			System.out.println("Connected with " + rs.getRow() + " states.");
+		} catch (SQLException e) {
+			System.err.println(e);
+		} finally {
+			if (conn != null)
+				conn.close();
+		}
 	}
 
 }
